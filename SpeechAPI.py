f931f86bf0fc6e1efcbc95f8aacd3ac72c314656
Ray <ray.dekruif@student.hu.nl>
2022-05-24 13:49:39 +0200

    API's
    API's can now use audio files and recognize

--- SpeechAPI.py
+++ SpeechAPI.py
@@ -1,52 +1,23 @@
-from statistics import mode
-from numpy import empty
 import speech_recognition as sr
-import json
-from sqlalchemy import false, true
 
-"""
-    Name of the class: SpeechToText
-
-    What it does:
-    Activates the speechAPI and sets the language.  
-"""
 class SpeechToText:
-    """
-        This function sets the language. 
-        @param language: is just a string like "Dutch" or "English"
-    """
-    def languageConverter(self,*,language):
-        '''This function converts the languages to the right format for the function: speechRecoginzer'''
-        switch_language = {
-            "Dutch": "nl-NL",
-            "English": "en-US",
-        }
-        return switch_language[language] 
-    """
-    Function for the speechRecognizer 
-    """
-    def google_api(self,*,language):
-        '''This function recognizes speech and prints out the text'''
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-                print("You can talk :) ")
-                audio_data = r.record(source, duration=5)
-                print("Recognizing...")
-                text = r.recognize_google(audio_data, language=self.languageConverter(language=language))
-                print(text.split())
-    
-    def api(self,*,language):
-        '''This function recognizes speech and prints out the text'''
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-                print("You can talk :) ")
-                audio_data = r.record(source, duration=5)
-                print("Recognizing...")
-                text = r.recognize_google(audio_data, language=self.languageConverter(language=language))
-                print(text.split())
-    
 
-        
+    def google_api(self,*,audio_file):
+        r = sr.Recognizer()
+        with sr.AudioFile(audio_file) as source:
+                audio_data = r.record(source)
+                print("File loading google... ")
+                text = r.recognize_google(audio_data, language="en-US")
+                return text.split()
+    
+    def sphinx_api(self,*,audio_file):
+        r = sr.Recognizer()
+        with sr.AudioFile(audio_file) as source:
+                audio_data = r.record(source)
+                print("File loading sphinx... ")
+                text = r.recognize_sphinx(audio_data)
+                return text.split()
+
 sp = SpeechToText()
-# sp.google_api(language="Dutch")
-sp.api()
+sp.google_api(audio_file="r2rd2_research/testEN.wav")
+sp.sphinx_api(audio_file="r2rd2_research/testEN.wav")
